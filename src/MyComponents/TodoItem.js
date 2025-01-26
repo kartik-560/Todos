cb1c6c37d60698140b166d7cca47d30d3c7efc25
kartik-560 <kartikkanzode@gmail.com>
2025-01-26 13:29:40 +0530

    Enhance styling and layout for the app; update TodoItem and Todos components for improved readability

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -4,8 +4,8 @@ export const TodoItem = ({todo, onDelete}) => {
     return (
         <>
         <div>
-           <h4>{todo.title}</h4>
-           <p>{todo.desc}</p>
+           <h4  >{todo.title}</h4>
+           <p  >{todo.desc}</p>
            <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button> 
         </div>
         <hr/> 
